d011a4e875809f62bd523d1b8b139a647c622d2e
Marcel Augusto <marcel.augusto.roxha@gmail.com>
2023-08-16 20:27:22 -0300

    Pagina para adicionar o valor no processo de venda quase finalizado ja esta pegando o valor cadastrado na loja e do ifood

--- app/src/main/java/com/marcel/a/n/roxha/deliciasdamamae/activity/AdicionarProdutoComoVendidoNoSistema.java
+++ app/src/main/java/com/marcel/a/n/roxha/deliciasdamamae/activity/AdicionarProdutoComoVendidoNoSistema.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,15 +31,32 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
 
     //COMPONENTES DE TELA
     private ImageView imagemDoProdutoNoProcessoDeVenda;
-
-    private TextView nomeRecuperadoDoProdutoNoProcessoDeVenda;
     private TextView textoNomeDoProdutoNoProcessoDeVenda;
+    private TextView nomeRecuperadoDoProdutoNoProcessoDeVenda;
+
+    private TextView textoValorVendaLojaDoProdutoNoProcessoDeVenda;
+    private TextView valorVendaLojaRecuperadoDoProdutoNoProcessoDeVenda;
+    private TextView textoValorVendaIfoodDoProdutoNoProcessoDeVenda;
+    private TextView valorVendaIfoodRecuperadoDoProdutoNoProcessoDeVenda;
+    private TextView textoValorQueVaiSerVendidoMesmo;
+
+
+
 
     private RadioGroup radioGroupEscolherValoresCadastrados;
-    private RadioGroup radioGroupEditarValorParaVender;
+
 
     private RadioButton radioButtonEscolherValoresCadastrados;
     private RadioButton radioButtonEditarValorParaVender;
+    private RadioButton radioButtonSelecionarPrecoDeVendaNaLoja;
+    private RadioButton radioButtonSelecionarPrecoDeVendaNoIfood;
+
+    private Button botaoConfirmar;
+    private Button botaoCancelar;
+    private TextView textoInformativoParaEscolherUmDosValoresCadastradosOu;
+    private Button botaoParaEditarValorParaVenda;
+
+    private TextInputEditText editValorQueFoiVendidoOProdutoNaHoraDaVenda;
 
     public AlertDialog progressDialogCarregando;
 
@@ -57,6 +76,8 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
     private String valorDoIfoodDoProdutoRecuperado;
     private String valorNaLojaDoProdutoRecuperado;
     private String valoQueFoiVendidoOProdutoRecuperado;
+    private boolean oValorFoiEditado = false;
+
 
     //CLASSES
     private BolosModel produtoRecuperadoPraExibirNaTela;
@@ -74,12 +95,28 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
         textoNomeDoProdutoNoProcessoDeVenda = findViewById(R.id.textView89);
 
 
-        radioGroupEscolherValoresCadastrados = findViewById(R.id.radioGroupExcolherValoresCadastrados);
-        radioGroupEditarValorParaVender = findViewById(R.id.radioGroupEditarValorDeVenda);
+        textoValorVendaLojaDoProdutoNoProcessoDeVenda = findViewById(R.id.textView96);
+        textoValorQueVaiSerVendidoMesmo = findViewById(R.id.textoValorQueVaiSerVendidoMesmoId);
+        valorVendaLojaRecuperadoDoProdutoNoProcessoDeVenda = findViewById(R.id.valorCadastradoParaVendaNaLojaProcessoDeVenda);
 
-        radioButtonEscolherValoresCadastrados = findViewById(R.id.idEscolherValoresCadastradosParaVenda);
-        radioButtonEditarValorParaVender = findViewById(R.id.idEditarOValoParaVender);
 
+        textoValorVendaIfoodDoProdutoNoProcessoDeVenda = findViewById(R.id.textView98);
+        valorVendaIfoodRecuperadoDoProdutoNoProcessoDeVenda = findViewById(R.id.valorCadastradoParaVendaNoIfoodProcessoDeVenda);
+
+
+
+
+        textoInformativoParaEscolherUmDosValoresCadastradosOu = findViewById(R.id.textoInformaParaEscolherUmDosValoresCadastradaosId);
+        botaoParaEditarValorParaVenda = findViewById(R.id.botaoParaEditarOValorNaHoraDaVenda);
+
+
+        radioGroupEscolherValoresCadastrados = findViewById(R.id.radioGroupEcolherValoresCadatradosNoProcessoDeVenda);
+        radioButtonSelecionarPrecoDeVendaNaLoja = findViewById(R.id.radioButtonPrecoCadastradoParaVendaNaLojaNoProcessoDeVenda);
+        radioButtonSelecionarPrecoDeVendaNoIfood = findViewById(R.id.radioButtonPrecoCadastradoParaVendaNoIfoodNoProcessoDeVenda);
+
+        botaoConfirmar = findViewById(R.id.botaoConfirmarVendaNoProcessoDeVenda);
+        botaoCancelar = findViewById(R.id.botaoCancelarProcessoDeVendaId);
+        carregarCargaDeDesaparecerConteudoDaTela();
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(AdicionarProdutoComoVendidoNoSistema.this);
@@ -93,7 +130,7 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
                 android.R.color.transparent
         );
         this.progressDialogCarregando.show();
-        carregarCargaDeDesaparecerConteudoDaTela();
+
 
         itemKey = getIntent().getStringExtra("itemKey");
 
@@ -116,8 +153,10 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
 
                             nomeRecuperadoDoProdutoNoProcessoDeVenda.setText(nomeDoProdutoRecuperado);
 
-
-
+                            valorVendaIfoodRecuperadoDoProdutoNoProcessoDeVenda.setText("R$: " + valorDoIfoodDoProdutoRecuperado);
+                            valorVendaLojaRecuperadoDoProdutoNoProcessoDeVenda.setText("R$: " + valorNaLojaDoProdutoRecuperado);
+                            radioButtonSelecionarPrecoDeVendaNaLoja.setText("Vendi pelo valor de R$: " + valorNaLojaDoProdutoRecuperado);
+                            radioButtonSelecionarPrecoDeVendaNoIfood.setText("Vendi pelo valor de R$: " + valorDoIfoodDoProdutoRecuperado);
 
 
 
@@ -138,32 +177,72 @@ public class AdicionarProdutoComoVendidoNoSistema extends AppCompatActivity {
         }
 
 
+        botaoParaEditarValorParaVenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oValorFoiEditado = true;
+                AlertDialog.Builder alert = new AlertDialog.Builder(AdicionarProdutoComoVendidoNoSistema.this);
+                alert.setTitle("EDITANDO O VALOR DE VENDA");
+                alert.setMessage("O valor editado aqui será processado nessa venda, digite o valor vendido e clique em CONFIRMAR");
+                alert.setIcon(R.drawable.ic_alertdialogatencao_24);
+                alert.setCancelable(false);
 
 
+            }
+        });
+
+        botaoConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        radioButtonSelecionarPrecoDeVendaNoIfood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoValorQueVaiSerVendidoMesmo.setText("Valor que será vendido é R$: " + valorDoIfoodDoProdutoRecuperado);
+            }
+        });
+
+        radioButtonSelecionarPrecoDeVendaNaLoja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoValorQueVaiSerVendidoMesmo.setText("Valor que será vendido é R$: " + valorNaLojaDoProdutoRecuperado);
+            }
+        });
 
     }
 
     private void carregarCargaDeDesaparecerConteudoDaTela(){
 
+        imagemDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
         nomeRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
         textoNomeDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
-        radioGroupEscolherValoresCadastrados.setVisibility(View.GONE);
-        radioGroupEditarValorParaVender.setVisibility(View.GONE);
-        radioButtonEscolherValoresCadastrados.setVisibility(View.GONE);
-        radioButtonEditarValorParaVender.setVisibility(View.GONE);
-
+        textoValorVendaLojaDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
+        valorVendaLojaRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
+        textoValorVendaIfoodDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
+        valorVendaIfoodRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.GONE);
+        radioButtonSelecionarPrecoDeVendaNaLoja.setVisibility(View.GONE);
+        radioButtonSelecionarPrecoDeVendaNoIfood.setVisibility(View.GONE);
+        botaoConfirmar.setVisibility(View.GONE);
+        botaoCancelar.setVisibility(View.GONE);
 
     }
 
     private void carregarCargaDeAparecimentoDoConteudoDaTela(){
-
+        imagemDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
         nomeRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
         textoNomeDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
+        textoValorVendaLojaDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
+        valorVendaLojaRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
+        textoValorVendaIfoodDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
+        valorVendaIfoodRecuperadoDoProdutoNoProcessoDeVenda.setVisibility(View.VISIBLE);
+        botaoConfirmar.setVisibility(View.VISIBLE);
+        botaoCancelar.setVisibility(View.VISIBLE);
         radioGroupEscolherValoresCadastrados.setVisibility(View.VISIBLE);
-        radioGroupEditarValorParaVender.setVisibility(View.VISIBLE);
-        radioButtonEscolherValoresCadastrados.setVisibility(View.VISIBLE);
-        radioButtonEditarValorParaVender.setVisibility(View.VISIBLE);
-
+        radioButtonSelecionarPrecoDeVendaNaLoja.setVisibility(View.VISIBLE);
+        radioButtonSelecionarPrecoDeVendaNoIfood.setVisibility(View.VISIBLE);
 
     }
 
